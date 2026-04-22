f303efe6f43292a55799f8776867ffae14741344
bed-com-35-23 <bed-com-35-23@unima.ac.mw>
2026-04-22 05:52:34 -0700

    Add files via upload

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,51 +1,62 @@
-#include "Rectangle.h"
-#include <iostream>
 
+
+#include <iostream>
 using namespace std;
 
-//constructor
-Rectangle::Rectangle() {
-length = 0.0f;
-width = 0.0f;
-}
-Rectangle::~Rectangle() {
-    // No dynamic memory to clean up
-}
-setters
-void Rectangle::setLength(float l) {
-    length = l;
-}
-void Rectangle::setWidth(float w) {
-    width = w;
-}
-//getters
-float Rectangle::getLength() const {
-    return length;
-}
-float Rectangle::getWidth() const {
-    return width;
-}
-//area function
-float Rectangle::getArea() const {
-    return length * width;
-}
+class Rectangle {
+private:
+    float length;
+    float width;
+
+public:
+    // Constructor
+    Rectangle() {
+        length = 0.0f;
+        width = 0.0f;
+    }
+
+    // Destructor
+    ~Rectangle() {}
+
+    // Setters
+    void setLength(float l) {
+        length = l;
+    }
+
+    void setWidth(float w) {
+        width = w;
+    }
+
+    // Getters
+    float getLength() const {
+        return length;
+    }
+
+    float getWidth() const {
+        return width;
+    }
+
+    // Area
+    float getArea() const {
+        return length * width;
+    }
+};
+
 int main() {
     Rectangle rect;
+
     float length, width;
 
-    //user input for length and width
-    cout << "Enter length of the rectangle: ";
+    cout << "Enter length: ";
     cin >> length;
-    cout << "Enter width of the rectangle: ";
+
+    cout << "Enter width: ";
     cin >> width;
 
     rect.setLength(length);
     rect.setWidth(width);
-    //out
 
-    cout << "Length: " << rect.getLength() << endl; // Length: 5
-    cout << "Width: " << rect.getWidth() << endl;   // Width: 3
-    cout << "Area: " << rect.getArea() << endl;        // Area: 15
+    cout << "Area: " << rect.getArea() << endl;
 
     return 0;
 }
